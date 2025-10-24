89c72b154e7c9e1dbcad49ec7e114cad6f490c60
Juan Camilo Betancur Mejia <41242233+jbetancurmejia@users.noreply.github.com>
2025-10-24 00:16:30 -0500

    Add files via upload

---
+++ chunk-7GUZWF3R.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"login",loadComponent:()=>import("./chunk-OE3V2Z2W.js").then(o=>o.LoginPage)},{path:"",redirectTo:"login",pathMatch:"full"}];export{t as AUTH_ROUTES};
---
+++ chunk-V6VYBFF7.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:()=>import("./chunk-W7SYSIBY.js").then(o=>o.HomePage)}];export{t as DASHBOARD_ROUTES};
