7d1283e6fcff01a86386264cf2dd7407a982e660
camilo betancur <jcbmdeveloper@gmail.com>
2025-10-23 23:25:57 -0500

    feat: actualizar GitHub Pages con mejoras UX/UI
    - Agregar logo móvil en login
    - Avatar con iniciales del usuario (AA)
    - Efectos hover en actividades recientes
    - Selector de idioma corregido para fondo claro
    - Mejorar .gitignore para excluir node_modules y archivos innecesarios

---
+++ browser/chunk-7HQ42NWH.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"login",loadComponent:()=>import("./chunk-2YRBCG6N.js").then(o=>o.LoginPage)},{path:"",redirectTo:"login",pathMatch:"full"}];export{t as AUTH_ROUTES};
